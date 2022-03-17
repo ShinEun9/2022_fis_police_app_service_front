7da5bf1215d9ffa18edccca6bd8695ffdd4f5b38
지상은 <chisang-eun@jisang-eun-ui-MacBookAir.local>
2022-03-17 16:22:40 +0900

    pull

--- components/molecule/CustomMap.js
+++ components/molecule/CustomMap.js
@@ -36,48 +36,48 @@ export default function CustomMap({c_latitude, c_longitude,c_name}) {
         longitudeDelta: LONGITUDE_DELTA,
     }
 
-    // const [agentLoc, setAgentLoc] = useState([{
-    //     key:-1,
-    //     coords:{
-    //         latitude:0,
-    //         longitude:0
-    //     }
-    // }])
-    //
-    // const getToken = async () => {
-    //     const t = await AsyncStorage.getItem("@token");
-    //     return t;
-    // }
-    //
-    // useEffect(() => {
-    //     setInterval(function (){
-    //         getToken().then((token) => {
-    //             getAgentLocation(token)
-    //         })
-    //     },3000)
-    // }, [])
-    //
-    // const getAgentLocation = async (token) => {
-    //     await axios.get(`http://54.175.8.114:8080/app/schedule/location`, {headers: {Authorization: `Bearer ${token}`}})
-    //         .then((res) => {
-    //             console.log("현장요원 위치")
-    //             console.log(res.data)
-    //             const buf = []
-    //             res.data.map((data, index) => {
-    //                 buf[index] = {
-    //                     key: data.agent_id,
-    //                     coords: {
-    //                         latitude: parseFloat(data.a_cur_lat),
-    //                         longitude: parseFloat(data.a_cur_long)
-    //                     }
-    //                 }
-    //             })
-    //             setAgentLoc(buf)
-    //             console.log("set")
-    //         }).catch((err) => {
-    //             console.log(err)
-    //         })
-    // }
+    const [agentLoc, setAgentLoc] = useState([{
+        key:-1,
+        coords:{
+            latitude:0,
+            longitude:0
+        }
+    }])
+
+    const getToken = async () => {
+        const t = await AsyncStorage.getItem("@token");
+        return t;
+    }
+
+    useEffect(() => {
+        setInterval(function (){
+            getToken().then((token) => {
+                getAgentLocation(token)
+            })
+        },3000)
+    }, [])
+
+    const getAgentLocation = async (token) => {
+        await axios.get(`http://54.175.8.114:8080/app/schedule/location`, {headers: {Authorization: `Bearer ${token}`}})
+            .then((res) => {
+                console.log("현장요원 위치")
+                console.log(res.data)
+                const buf = []
+                res.data.map((data, index) => {
+                    buf[index] = {
+                        key: data.agent_id,
+                        coords: {
+                            latitude: parseFloat(data.a_cur_lat),
+                            longitude: parseFloat(data.a_cur_long)
+                        }
+                    }
+                })
+                setAgentLoc(buf)
+                console.log("set")
+            }).catch((err) => {
+                console.log(err)
+            })
+    }
 
 
 
@@ -88,13 +88,13 @@ export default function CustomMap({c_latitude, c_longitude,c_name}) {
         // </MapView>
         <MapView style={styles.map} region={example} loadingEnabled provider={PROVIDER_GOOGLE}>
             <Marker coordinate={example} title={c_name}/>
-            {/*{agentLoc.map((data, index) => {*/}
-            {/*    return <Marker key={data.key} coordinate={data.coords} />*/}
-            {/*})}*/}
+            {agentLoc.map((data, index) => {
+                return <Marker key={data.key} coordinate={data.coords} />
+            })}
 
-            {console.log("되나?")}
-            <CustomMarker/>
-            {console.log("된다?")}
+            {/*{console.log("되나?")}*/}
+            {/*<CustomMarker/>*/}
+            {/*{console.log("된다?")}*/}
         </MapView>
 
 
