b555b5ec2310831a6e7504f028427451e75b6b6d
지상은 <chisang-eun@jisang-eun-ui-MacBookAir.local>
2022-03-28 10:36:21 +0900

    은수를 위해

--- components/template/ApplyCenterTemplate.js
+++ components/template/ApplyCenterTemplate.js
@@ -51,7 +51,23 @@ function ApplyCenterTemplate(props) {
                 console.log("신청현황")
                 console.log(res.data.data)
                 setIsLoading(false)
-                setApplyData(res.data.data)
+                let buf=[]
+                res.data.data.map((data,index)=>{
+                    if(data.accept==="accept"){
+                        buf[index]={
+                            ...data,
+                            accept:"참여"
+                        }
+                    }
+                    else{
+                        buf[index]={
+                            ...data,
+                            accept:"미참여"
+                        }
+                    }
+                })
+                let rBuf=buf.reverse()
+                setApplyData(rBuf)
             }).catch((err) => {
                 console.log(err)
                 setIsLoading(false)
@@ -80,51 +96,39 @@ function ApplyCenterTemplate(props) {
             <View style={{flex: 0.6, zIndex: 1, paddingTop: Platform.OS === 'ios' ? 0 : 30,}}>
                 <CustomNavigation props={props} type="CenterTitleNavbar" title="지문 등록 신청하러 가기"/>
             </View>
-            <ScrollView>
-                <View style={{flex:9,marginTop:60}}>
+                <View style={{flex:6,marginTop:60}}>
                     <View style={{flex:1,alignItems: "center",marginBottom:10}}>
-                        <View style={{
-                            width: useWindowDimensions().width * 0.95,
-                            height: useWindowDimensions().width*0.85,
-                            backgroundColor: Style.color3,
-                            justifyContent: "center",
-                            alignItems: "center",
-                            flex:5
-                        }}>
+                        <View style={styles.list}>
                             <View>
                                 <Text style={{fontSize: Platform.OS==="ios"?30:24,paddingVertical:20}}>최근 신청 현황</Text>
                             </View>
-                            <ScrollView >
-                                {applyData.map((data, index) => {
-                                    if (index < 5) {
-                                        return <View key={index}
-                                                     style={{...styles.container2}}>
-                                            <Text style={{fontSize: Platform.OS==="ios"?19:17}}>지문 등록 참여 여부 : {data.accept}</Text>
-                                            <Text style={{fontSize: Platform.OS==="ios"?19:17}}>지문 등록 희망 날짜 : {data.h_date}</Text>
-                                        </View>
-                                    }
-                                })}
-                            </ScrollView>
+                                <ScrollView  >
+                                    {applyData.map((data, index) => {
+                                        if (index < 5) {
+                                            return <View key={index}
+                                                         style={{...styles.container2}}>
+                                                <Text style={{fontSize: Platform.OS==="ios"?19:17}}>지문 등록 참여 여부 : {data.accept}</Text>
+                                                <Text style={{fontSize: Platform.OS==="ios"?19:17}}>지문 등록 희망 날짜 : {data.h_date}</Text>
+                                            </View>
+                                        }
+                                    })}
+                                </ScrollView>
                         </View>
                     </View>
-                    <View style={{...styles.Guide,flex:1}}>
                         <View style={{paddingVertical:70}}>
-                            <View style={{alignItems: 'center', justifyContent: 'center',marginBottom:10}}>
-                                <Text style={styles.text}>1. 본인의 기본 정보가 맞는지 확인한다.</Text>
-                                <Text style={styles.text}>2. 지문 등록 참여 여부를 선택한다.</Text>
-                                <Text style={styles.text}>3. 희망하는 날짜를 선택한다.</Text>
-                                <Text style={styles.text}>( 추후 전화를 통해 방문 날짜 확정 예정 )</Text>
-                                <Text style={styles.text}>4. 제출 버튼을 눌러 신청서를 제출한다.</Text>
-                            </View>
+                            {/*<View style={{alignItems: 'center', justifyContent: 'center',marginBottom:10}}>*/}
+                            {/*    <Text style={styles.text}>1. 본인의 기본 정보가 맞는지 확인한다.</Text>*/}
+                            {/*    <Text style={styles.text}>2. 지문 등록 참여 여부를 선택한다.</Text>*/}
+                            {/*    <Text style={styles.text}>3. 희망하는 날짜를 선택한다.</Text>*/}
+                            {/*    <Text style={styles.text}>( 추후 전화를 통해 방문 날짜 확정 예정 )</Text>*/}
+                            {/*    <Text style={styles.text}>4. 제출 버튼을 눌러 신청서를 제출한다.</Text>*/}
+                            {/*</View>*/}
                             <View style={{justifyContent: 'center', alignItems: 'center',paddingVertical:20}}>
                                 <CustomButton width={screen.width * 0.75} height={Platform.OS==="ios"?40:35}
                                               backgroundColor={Style.color2} onPress={onPressFunc} content={"지문 등록 신청하러 가기"}/>
                             </View>
                         </View>
-
-                    </View>
                 </View>
-            </ScrollView>
             <Modal
                 isVisible={modalVisible}
                 useNativeDriver={true}
@@ -164,12 +168,6 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems:"center"
     },
-    Guide: {
-        width: Dimensions.get("window").width*0.95,
-        height:"auto",
-        alignItems: "center",
-        backgroundColor: Style.color3
-    },
     modalContainer: {
         flexDirection: "column",
         alignItems: "center",
@@ -195,4 +193,12 @@ const styles = StyleSheet.create({
         height: "auto",
         marginBottom: 20
     },
+    list:{
+        width: screen.width * 0.95,
+        height: "auto",
+        backgroundColor: Style.color3,
+        justifyContent: "center",
+        alignItems: "center",
+        flex:5
+    }
 })
